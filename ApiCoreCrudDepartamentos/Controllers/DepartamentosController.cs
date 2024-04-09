45292b5e1c2a3b402c9b580a93b8941adafa2151
serraguti <pacoserranox@gmail.com>
2024-04-09 09:39:00 +0200

    Add project files.

---
+++ ApiCoreCrudDepartamentos/Controllers/DepartamentosController.cs
@@ -0,0 +1,94 @@
+﻿using ApiCoreCrudDepartamentos.Models;
+using ApiCoreCrudDepartamentos.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCoreCrudDepartamentos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartamentosController : ControllerBase
+    {
+        private RepositoryDepartamentos repo;
+
+        public DepartamentosController(RepositoryDepartamentos repo)
+        {
+            this.repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Departamento>>>
+            GetDepartamentos()
+        {
+            return await this.repo.GetDepartamentosAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Departamento>>
+            FindDepartamento(int id)
+        {
+            return await this.repo.FindDepartamentoAsync(id);
+        }
+
+        //LOS METODOS POR DEFECTO DE POST O PUT, RECIBEN UN OBJETO
+        //SI QUISIERAMOS RECIBIR LOS DATOS POR PARAMETROS, 
+        //DEBEMOS UTILIZAR Route
+        //PODEMOS PERSONALIZAR LA RESPUESTA EN EL CASO QUE NO NOS 
+        //GUSTE ALGO, PUDIENDO DEVOLVER NotFound, BadRequest
+        [HttpPost]
+        public async Task<ActionResult> PostDepartamento
+            (Departamento departamento)
+        {
+            await this.repo.InsertDepartamentoAsync(departamento.IdDepartamento
+                , departamento.Nombre, departamento.Localidad);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            //PODEMOS PERSONALIZAR LA RESPUESTA
+            if (await this.repo.FindDepartamentoAsync(id) == null)
+            {
+                //NO EXISTE EL DEPARTAMENTO PARA ELIMINARLO
+                return NotFound();
+            }
+            else
+            {
+                await this.repo.DeleteDepartamentoAsync(id);
+                return Ok();
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> PutDepartamento
+            (Departamento departamento)
+        {
+            await this.repo.UpdateDepartamentoAsync(departamento.IdDepartamento
+                , departamento.Nombre, departamento.Localidad);
+            return Ok();
+        }
+
+        //PODEMOS TENER TODOS LOS METODOS POST/PUT/DELETE QUE DESEEMOS
+        //DEBEMOS PERSONALIZARLOS CON Route
+        [HttpPost]
+        [Route("[action]/{id}/{nombre}/{localidad}")]
+        public async Task<ActionResult> InsertParams
+            (int id, string nombre, string localidad)
+        {
+            await this.repo.InsertDepartamentoAsync(id, nombre, localidad);
+            return Ok();
+        }
+
+        //TAMBIEN PODEMOS COMBINAR RECIBIR OBJETOS CON ROUTES
+        [HttpPut]
+        [Route("[action]/{id}")]
+        public async Task<ActionResult> UpdateParams
+            (int id, Departamento departamento)
+        {
+            await this.repo.UpdateDepartamentoAsync(id,
+                departamento.Nombre, departamento.Localidad);
+            return Ok();
+        }
+    }
+}
---
+++ ApiCoreCrudDepartamentos/Program.cs
@@ -0,0 +1,48 @@
+using ApiCoreCrudDepartamentos.Data;
+using ApiCoreCrudDepartamentos.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Net.Http.Headers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddTransient<RepositoryDepartamentos>();
+string connectionString =
+    builder.Configuration.GetConnectionString("SqlAzure");
+builder.Services.AddDbContext<DepartamentosContext>
+    (options => options.UseSqlServer(connectionString));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Hoy tenemos Champions",
+        Description = "Api Crud de Champions"
+    });
+});
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint(url: "/swagger/v1/swagger.json"
+        , name: "Api Crud Champions v1");
+    options.RoutePrefix = "";
+});
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
